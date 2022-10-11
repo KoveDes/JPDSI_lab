50d3a9bb650e0c40603fa770346cfcadcbad0a86
racus <racus@DESKTOP-3BIJPJ6.mshome.net>
2022-10-11 14:01:10 +0200

    lab_1

--- src/main/java/com/jsfcourse/calc/CreditBB.java
+++ src/main/java/com/jsfcourse/calc/CreditBB.java
@@ -11,16 +11,57 @@ import javax.faces.context.FacesContext;
 @RequestScoped
 //@SessionScoped
 public class CreditBB {
-	private String x;
-	private String y;
+	private String amount;
+	private String time;
+	private String rate;
 	private Double result;
-	//kwota, oprocentowanie, lat -> rata
-
+	
 	@Inject
 	FacesContext ctx;
 
+	public String getAmount() {
+		return amount;
+	}
+
+	public void setAmount(String amount) {
+		this.amount = amount;
+	}
+
+	public String getTime() {
+		return time;
+	}
+
+	public void setTime(String time) {
+		this.time = time;
+	}
+
+	public String getRate() {
+		return rate;
+	}
+
+	public void setRate(String rate) {
+		this.rate = rate;
+	}
+
+	public Double getResult() {
+		return result;
+	}
+
+	public void setResult(Double result) {
+		this.result = result;
+	}
+
+	
 	public String calc() {
+		//konwersja
+		int amount = Integer.parseInt(this.amount);
+		int time = Integer.parseInt(this.time);
+		double rate = Double.parseDouble(this.rate);
 		// obliczenia
-		return null;
+		double n = time * 12;
+		double r = rate/100;
+		this.result = amount * ((r / 12 * Math.pow((1+r/12),n))) / ((Math.pow((1 + r/12),n)-1));
+//		String resultFinal = String.valueOf(this.result);
+		return "showresult";
 	}
 }
